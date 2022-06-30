392fee532de88f375722c4fd5bb286e097abcd99
Joseph-Mukuthi <joseph.mukuthi@student.moringaschool.com>
2022-06-30 15:20:30 +0300

    Add names,email and robots.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './Card';
 import 'tachyons';
+import {robots} from './robots';
+import Cardlist from './Cardlist';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </div>
+    <Cardlist robots={robots}/>
   </React.StrictMode>
 );
 
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id : 1,
+    name : 'Joseph Mukuthi',
+    username : 'J.Mukuthi22',
+    email : 'jmukuthi@yahoo.com'
+  },
+  {
+    id : 2,
+    name : 'Mildred Wangui',
+    username : 'BossLady',
+    email : 'm-wangui@yahoo.com'
+  },
+  {
+    id : 3,
+    name : 'Alex Kimeu',
+    username : 'Kimz',
+    email : 'Kimazo@gmail.com'
+  },
+  {
+    id : 4,
+    name : 'Ian Mwenda',
+    username : 'Mwenda Boy',
+    email : 'MwendaIan@hotmail.com'
+  },
+  {
+    id : 5,
+    name : 'Hillary Okoth',
+    username : 'OkothIsm',
+    email : 'H_okoth@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
