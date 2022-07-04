610589ccb21d8af9ce6984ec569f2a8ff37c301f
Joseph-Mukuthi <joseph.mukuthi@student.moringaschool.com>
2022-07-04 14:17:19 +0300

    Add containers and components folder and move files to them. Also add Scroll components and robots API.

---
+++ src/containers/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import Cardlist from '../components/Cardlist';
+import SearchBox from '../components/SearchBox';
+import './App.css';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+
+class App extends Component {
+  constructor(){
+    super()
+      this.state = {
+        robots: [],
+        searchField: ''
+      }
+  }
+
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({searchField: event.target.value})
+     }
+
+  render(){
+    const {robots, searchField} = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    })
+    // Use ternary operator for destructuring
+    return !robots.length ? 
+      <h1 className='tc f4'>Loading...</h1> :
+       (
+        <div className='tc f2'>
+          <h1>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <ErrorBoundry>
+              <Cardlist robots={filteredRobots} />
+            </ErrorBoundry>
+          </Scroll>
+        </div>
+      );
+  }
+}
+
+
+export default App;
